4ffd7eb4e3b1a465c5e8d38382a52cfe5a884d8c
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-10-03 10:17:17 +0700

    build(fix): remove golang module handling for non existant files

--- .bin/release-hook-prerelease.sh
+++ .bin/release-hook-prerelease.sh
@@ -11,9 +11,6 @@ fi
 hugo mod get -u ./...
 hugo mod tidy
 
-git add go.mod
-git add go.sum
-
 if test -f "$SCRIPTPATH"/replacements; then
   while read -ra __; do
     go mod edit -replace ${__[0]}=${__[1]}
