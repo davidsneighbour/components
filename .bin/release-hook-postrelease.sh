a797d36a3f70c39cbcd00fd8943c4d029e0a4270
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-10-03 11:34:13 +0700

    build: add tags for all components after release

--- .bin/release-hook-postrelease.sh
+++ .bin/release-hook-postrelease.sh
@@ -1,3 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+# declare path to this script
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 || exit ; pwd -P )"
+
+# retrieve new version number
+NEW_VERSION="$(cat ".version")"
+
+# tag version numbers for all componnets
+if test -f "$SCRIPTPATH"/components; then
+  while read -ra __; do
+    git tag "${__[0]}/v${NEW_VERSION}"    
+  done < "$SCRIPTPATH"/components
+fi
+
+# send all tags to remote
 git push --follow-tags origin main
