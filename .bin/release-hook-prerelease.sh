15de04b914804f0f192b78e9c47ad1ee9ef77a96
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-10-03 10:12:55 +0700

    build: add components golang module files to release process

--- .bin/release-hook-prerelease.sh
+++ .bin/release-hook-prerelease.sh
@@ -14,12 +14,17 @@ hugo mod tidy
 git add go.mod
 git add go.sum
 
-#rm -rf public
-#hugo
-#git add public/ -f
-
 if test -f "$SCRIPTPATH"/replacements; then
   while read -ra __; do
     go mod edit -replace ${__[0]}=${__[1]}
   done < "$SCRIPTPATH"/replacements
+fi
+
+if test -f "$SCRIPTPATH"/components; then
+  while read -ra __; do
+    GOMOD=${__[0]}/go.mod
+    if test -f "$GOMOD"; then git add "$GOMOD"; fi;
+    GOSUM=${__[0]}/go.sum
+    if test -f "$GOSUM"; then git add "$GOSUM"; fi;
+  done < "$SCRIPTPATH"/components
 fi
