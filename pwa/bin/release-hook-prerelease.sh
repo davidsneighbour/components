54f7719aae1c00f453798a6ed12915595bf93015
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-11-28 18:34:04 +0700

    chore(release): pwa/v1.4.15

--- pwa/bin/release-hook-prerelease.sh
+++ pwa/bin/release-hook-prerelease.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-# declare path to this script
-SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 || exit ; pwd -P )"
-
-# remove replacement scripts
-if test -f "$SCRIPTPATH"/replacements; then
-  while read -ra __; do
-    go mod edit -dropreplace "${__[0]}"
-  done < "$SCRIPTPATH"/replacements
-fi
-
 hugo mod get -u ./...
 hugo mod tidy
 
@@ -17,11 +7,4 @@ hugo mod tidy
 npm run build
 git add static
 
-if test -f "$SCRIPTPATH"/replacements; then
-  while read -ra __; do
-    go mod edit -replace "${__[0]}"="${__[1]}"
-  done < "$SCRIPTPATH"/replacements
-fi
-
-if test -f go.mod; then git add "$GOMOD"; fi;
-if test -f go.sum; then git add "$GOSUM"; fi;
+git add go.mod
